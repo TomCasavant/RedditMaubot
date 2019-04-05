0ef12e47b7c986d8a9f9fd4262ad7a0a6f16105d
Tom Casavant <tfcasavant@gmail.com>
2019-04-05 16:33:54 -0400

    Added ability to determine if sub exists or not

--- reddit.py
+++ reddit.py
@@ -3,17 +3,22 @@ from maubot import Plugin, MessageEvent
 from maubot.handlers import command
 from typing import List, Tuple
 from random import choice
-
+import requests
+from http.client import responses
 
 class RedditPlugin(Plugin):
-	@command.passive("(r/)([a-zA-Z]+)", multiple=True)
-	async def handler(self, evt: MessageEvent, subs: List[Tuple[str,str]]) -> None:
-		await evt.mark_read()
-		subreddits = []
-		for _, r, sub_str in subs:
-			subreddits.append("https://reddit.com/r/{}".format(sub_str))
+    @command.passive("(r/)([a-zA-Z]+)", multiple=True)
+    async def handler(self, evt: MessageEvent, subs: List[Tuple[str,str]]) -> None:
+        await evt.mark_read()
+        subreddits = [] #List of all subreddits given by user
+        for _, r, sub_str in subs:
+            link = 'https://reddit.com/r/{}'.format(sub_str)
+            r = requests.head(link, headers={'User-agent':'redditmaubot'}, allow_redirects=True)
+            if ("/r/" in r.url):
+                #Check if subreddit exists
+                subreddits.append(link)
 
-		condescending = ["Don't you mean {} ?", "Shouldn't this be {} ?", "Are you sure it isn't {} ?", "Isn't it {} ?", "uh, {} right?", "{} is probably what you're looking for"]
-		response = choice(condescending).format("\n".join(subreddits))
-		await evt.reply(response, html_in_markdown=True)
-
+        if len(subreddits) > 0:
+            condescending = ["Don't you mean {} ?", "Shouldn't this be {} ?", "Are you sure it isn't {} ?", "Isn't it {} ?", "uh, {} right?", "{} is probably what you're looking for"]
+            response = choice(condescending).format("\n".join(subreddits))
+            await evt.reply(response, html_in_markdown=True) #Reply to user
