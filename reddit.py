ed4f6cb5eda72d1505267146204db4949dc97635
Tom Casavant <tfcasavant@gmail.com>
2019-04-08 13:48:13 -0400

    Switched from requests to aiohttps (self.http)

--- reddit.py
+++ reddit.py
@@ -1,23 +1,36 @@
-from mautrix.types import EventType
+from random import choice
+from typing import List, Tuple
 from maubot import Plugin, MessageEvent
 from maubot.handlers import command
-from typing import List, Tuple
-from random import choice
-import requests
+
 
 class RedditPlugin(Plugin):
     @command.passive("(r/)(.+)", multiple=True)
-    async def handler(self, evt: MessageEvent, subs: List[Tuple[str,str]]) -> None:
+    async def handler(self, evt: MessageEvent, subs: List[Tuple[str, str]]) -> None:
         await evt.mark_read()
-        subreddits = [] #List of all subreddits given by user
-        for _, r, sub_str in subs:
-            link = 'https://reddit.com/r/{}'.format(sub_str)
-            r = requests.head(link, headers={'User-agent':'redditmaubot'}, allow_redirects=True)
-            if ("/r/" in r.url and r.status_code != 404):
-                #Check if subreddit exists
+        subreddits = []  # List of all subreddits given by user
+        for _, rslash, sub_str in subs:
+            link = "https://reddit.com/r/{}".format(sub_str)
+
+            async with self.http.head(
+                link, headers={"User-agent": "redditmaubot"}, allow_redirects=True
+            ) as response:
+                # Save url and status code of url
+                url = str(response.url)
+                status = response.status
+
+            if "/r/" in str(url) and status != 404:
+                # Check if subreddit exists
                 subreddits.append(link)
 
-        if len(subreddits) > 0:
-            condescending = ["Don't you mean {} ?", "Shouldn't this be {} ?", "Are you sure it isn't {} ?", "Isn't it {} ?", "uh, {} right?", "{} is probably what you're looking for"]
+        if subreddits:
+            condescending = [
+                "Don't you mean {} ?",
+                "Shouldn't this be {} ?",
+                "Are you sure it isn't {} ?",
+                "Isn't it {} ?",
+                "uh, {} right?",
+                "{} is probably what you're looking for",
+            ]
             response = choice(condescending).format("\n".join(subreddits))
-            await evt.reply(response, html_in_markdown=True) #Reply to user
+            await evt.reply(url, html_in_markdown=True)  # Reply to user
