97cdd62861c051d84fbd43eba144d6eb7e412f2b
Tom Casavant <tfcasavant@gmail.com>
2022-11-13 20:52:22 -0500

    Initial setup for reddit url

--- reddit.py
+++ reddit.py
@@ -3,15 +3,29 @@ from typing import List, Tuple
 import urllib.parse
 from maubot import Plugin, MessageEvent
 from maubot.handlers import command
+from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
 
+# Setup config file
+class Config(BaseProxyConfig):
+    def do_update(self, helper: ConfigUpdateHelper) -> None:
+        helper.copy("reddit_base")
 
 class RedditPlugin(Plugin):
+    async def start(self) -> None:
+        await super().start()
+        self.config.load_and_update()
+
+    @classmethod
+    def get_config_class(cls) -> Type[BaseProxyConfig]:
+        return Config
+
     @command.passive("((^| )r\/)([^\s^,^.]+)", multiple=True)
     async def handler(self, evt: MessageEvent, subs: List[Tuple[str, str]]) -> None:
         await evt.mark_read()
         subreddits = []  # List of all subreddits given by user
         for _, r_slash, __, sub_str in subs:
-            link = "https://reddit.com/r/{}".format(urllib.parse.quote(sub_str.lower()))
+            base_url = self.config["reddit_base"]
+            link = "https://{}/r/{}".format(base_url, urllib.parse.quote(sub_str.lower()))
 
             async with self.http.head(
                 link, headers={"User-agent": "redditmaubot"}, allow_redirects=True
